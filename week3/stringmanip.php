c56333a2d8382fa70aa28ebea32133b9b50faa84
Matt <mmarkwald01@gmail.com>
2017-09-11 16:20:41 -0400

    Reformatting
    Reformatting the output variable

--- week3/stringmanip.php
+++ week3/stringmanip.php
@@ -1,6 +1,16 @@
 <?php 
-	$myString = "\t <p>My children were pretending they work at the bank. My daughter said, \"Pay up Mr. O'Doyle, or we will take all of your stuff, even your Mom's shoes.\" My son said \"How much money do I owe you?\" to which my daughter replied \"$1,000,000.52. I have told you many times that interest was building up. You always said 'I know, but I have to pay for my Mom's shoes first.' but now you must pay me or face the consequences.\"</p> 
-		\n\t<p>My son replied \"Sigh...women are always taking my money.\"</p>";
-
+	$myString <<<MYOUTPUT 
+		<p>
+			My children were pretending they work at the bank. My daughter said, \"Pay
+			up Mr. O'Doyle, or we will take all of your stuff, even your Mom's shoes.
+			my son said \"How much money do I owe you?\" to which my daughter replied
+			$1,000,000.52. I have told you many times that interest was building up. You
+			always said 'I know, but I have to pay for my Mom's shoes first.' but now
+			you must pay me or face the consequences.
+		</p> 
+		<p>
+			My son replied Sigh...women are always taking my money.
+		</p>";
+MYOUTPUT
 	echo $myString;
 ?>
