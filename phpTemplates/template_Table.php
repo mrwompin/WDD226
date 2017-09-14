d9899420e67cdc76ffacb8d74b7ecd8434e563da
Matt <mmarkwald01@gmail.com>
2017-09-14 18:27:32 -0400

    Table Template

---
+++ phpTemplates/template_Table.php
@@ -0,0 +1,28 @@
+<table>
+	<thead>
+		<?php 
+			echo $get_Thead()
+		?>
+	</thead>
+	<tbody>
+		<?php 
+			echo $get_Tbody()
+		?>
+	</tbody>
+	<tfoot>
+		<?php 
+			echo $get_Foot()
+		?>
+	</tfoot>
+</table>
+
+<?php 
+	include "../libraries/HtmlLib.php"
+	class Table extends Tools
+	{
+		
+	}
+
+
+
+?>
