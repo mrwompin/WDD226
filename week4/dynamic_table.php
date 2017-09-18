0b8d5f93483300dbd4ea00dbeaec52fc0435978c
Matt <mmarkwald01@gmail.com>
2017-09-18 01:48:36 -0400

    assignment complete

--- week4/dynamic_table.php
+++ week4/dynamic_table.php
@@ -1,41 +1,79 @@
 <?php
-#DOC: DYNAMIC TABLE EXAMPLE
-	/** 
-	 * creates a table with dynamically generated numbered input as data
-	 *
-	 * creates a new instance of class table, sets an initial 
-	 * variable $cellNum to the number the function should start
-	 * the cell count on. the table loops through creating 4 rows
-	 * of output and using post incremented $cellNum variables to 
-	 * set the number for each cell. these rows are added using the 
-	 * table classes addrow() function which adds arrays of input to
-	 * the table $rows property which itself is an array. Then the 
-	 * build() function is called which creates opeing and closing tags
-	 * for the table and any rows in the $rows property.
-	 * 
-	*/
-# /*TOGGLE
-	$table = new Table; //new instance of class table
-	$cellNum = 1; //used for naming cells
 
-	echo "<!DOCTYPE html>\n";
-	echo "<html>\n";
-	echo "	<head>\n";
-	echo "		<title></title>\n";
-	echo "<style>";
-	
-	echo "</style>";
-	echo "	</head>\n";
-	echo "	<body>\n";
-	for($i=0;$i<4;$i++) {
-		$table->addRow([$cellNum++, $cellNum++, $cellNum++]); //each row uses the cellNum and increments it for the next one
+	week_4::html_open();
+	week_4::Run_Assignment(4,3,1);
+	echo "<br>";
+	week_4::Run_Assignment(5,5,-10);
+	echo "<br>";
+	week_4::Run_Assignment(25,25,1);
+	week_4::html_end();
+
+	class Week_4 {
+	#DOC: DYNAMIC TABLE EXAMPLE
+		/** 
+		 * creates a table with dynamically generated numbered input as data
+		 *
+		 * creates a new instance of class table, sets an initial 
+		 * variable $cellNum to the number the function should start
+		 * the cell count on. the table loops through creating 4 rows
+		 * of output and using post incremented $cellNum variables to 
+		 * set the number for each cell. these rows are added using the 
+		 * table classes addrow() function which adds arrays of input to
+		 * the table $rows property which itself is an array. Then the 
+		 * build() function is called which creates opeing and closing tags
+		 * for the table and any rows in the $rows property.
+		 * 
+		*/
+	# /*TOGGLE
+			public static function Run_Assignment($numRows, $numCols, $start){
+				$table = new Table; //new instance of class table;
+			 	$cellNum = $start; //used for naming cells
+			 	$row = [];
+				for($x=0;$x<$numRows;$x++) {
+					$row = [];
+					for($y=0;$y<$numCols;$y++) {
+						$row[$y]=$cellNum++;
+					}
+					$table->addRow($row);
+					unset($row);
+				}	
+				echo $table->build();
+			}
+	 /*		
+				for($i=0;$i<$row_num;$i++) {
+					$table->addRow([$cellNum++, $cellNum++, $cellNum++]); //each row uses the cellNum and increments it for the next one
+				}
+		 */
+		 #		echo $table->addRow(["Dynamic Table"],"header"); //adds a header, still need to figure out how to pass a colspan argument efficiently
+		#*/	
+			
+			public static function html_open() {
+					echo "<!DOCTYPE html>\n";
+					echo "<html>\n";
+					echo "	<head>\n";
+					echo "		<title>Dynamic Table</title>\n";
+					echo "<style>\n";
+					echo "	table, th, td {\n";
+					echo "		text-align: center;\n";
+					echo "		border-collapse: collapse;\n";
+					echo "		border: 1px gray solid;\n";
+					echo "	}\n";
+					echo "	td {\n";
+					echo " 		width: 100px;\n";
+					echo "	}\n";
+					echo "	tr.color {\n";
+					echo "		background-color: lightblue;\n";
+					echo "	}\n";
+					echo "</style>\n";
+					echo "	</head>\n";
+					echo "	<body>\n";	
+				}
+				public static function html_end() {
+					echo "	</body>\n"; // end my closing tags
+					echo "</html>\n";	
+				}
+			
 	}
- #	echo $table->addRow(["Dynamic Table"],"header"); //adds a header, still need to figure out how to pass a colspan argument efficiently
-	echo $table->build(); //build function uses the rows it was given to create the table
-	echo "	</body>\n";
-	echo "</html>\n";
-#*/
-
 #DOC: Class Table 
 	/** 
 	 * Table creates a table object
@@ -91,10 +129,10 @@
 		*/
 #	 /*TOGGLE
 		public function build(){
-			$this->htmlString = "<table>\n";
+			$this->htmlString = "		<table>\n";
 			$this->htmlString .= $this->constructHeader();
 			$this->htmlString .= $this->constructRow();
-			$this->htmlString .= "</table>\n";
+			$this->htmlString .= "		</table>\n";
 			$return = $this->htmlString;
 			self::$rows = [];
 			$this->numRows = null;
@@ -105,11 +143,11 @@
 		private function constructHeader() {
 			if (isset(self::$rows["header"])) {
 				$header = self::$rows["header"];
-				$return = "	<tr>\n";
+				$return = "			<tr>\n";
 				foreach($header as $th) {
-					$return .= "		<th>$th</th>\n";
+					$return .= "				<th>$th</th>\n";
 				}
-				$return .= "	</tr>\n";
+				$return .= "			</tr>\n";
 				return $return;
 			}
 		}
@@ -132,11 +170,19 @@
 			private function constructRow() {
 					$return = null;
 					for($i = 0; $i < $this->numRows; $i++) { 
-						$return .= "	<tr>\n";
-						foreach(self::$rows[$i] as $td) { //creates the <td></td> tags for the row values
-							$return .= "		<td>$td</td>\n";
+						if($i%2==0){
+							$return .= "			<tr>\n";
+							foreach(self::$rows[$i] as $td) { //creates the <td></td> tags for the row values
+							$return .= "				<td>$td</td>\n";
+							}
+						}	
+						else{
+							$return .= "			<tr class=\"color\">\n";
+							foreach(self::$rows[$i] as $td) { //creates the <td></td> tags for the row values
+							$return .= "				<td>$td</td>\n";
+							}
 						}
-						$return .= "	</tr>\n";
+						$return .= "			</tr>\n";
 					}
 					return $return;
 				} //Your Code Here	
@@ -144,5 +190,6 @@
 
 } 
 #*/
+
 
 ?>
